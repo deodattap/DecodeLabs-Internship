d9ec517c842815b7f3b619eec67debbedf4891bd
Deodatta Pagar <p.deodatta005@gmail.com>
2026-03-03 14:28:15 +0530

    Completed task 2

--- project-2-backend-api/server.js
+++ project-2-backend-api/server.js
@@ -1,47 +1,50 @@
-const express = require("express");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
-const PORT = 5000;
-app.use(express.json());
+const app = express()
+const PORT = 5000
+
+app.use(cors())
+app.use(express.json())
 
 let products = [
-    { id: 1, name: "Lipstick", price: 350 },
-    { id: 2, name: "Powder", price: 420 },
-    { id: 3, name: "Sunscreen", price: 480 }
-];
+  { id: 1, name: "Lipstick", price: 350 },
+  { id: 2, name: "Powder", price: 420 },
+  { id: 3, name: "Sunscreen", price: 480 }
+]
 
 app.get("/", (req, res) => {
-    res.send("Backend API is running");
-});
+  res.send("Backend API is running")
+})
 
-// GET products
 app.get("/products", (req, res) => {
-    res.status(200).json(products);
-});
+  res.status(200).json(products)
+})
 
-// POST product
 app.post("/products", (req, res) => {
-    const { name, price } = req.body;
 
-    if (!name || !price) {
-        return res.status(400).json({ message: "Name and price required" });
-    }
+  const name = req.body.name
+  const price = req.body.price
 
-    if (price <= 0) {
-        return res.status(400).json({ message: "Price must be positive" });
-    }
+  if(!name || !price){
+    return res.status(400).json({ message: "Name and price required" })
+  }
 
-    const newProduct = {
-        id: products.length + 1,
-        name,
-        price
-    };
+  if(price <= 0){
+    return res.status(400).json({ message: "Price must be positive" })
+  }
 
-    products.push(newProduct);
+  const newProduct = {
+    id: products.length + 1,
+    name: name,
+    price: price
+  }
 
-    res.status(201).json(newProduct);
-});
+  products.push(newProduct)
+
+  res.status(201).json(newProduct)
+})
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+  console.log("Server running at http://localhost:" + PORT)
+})
